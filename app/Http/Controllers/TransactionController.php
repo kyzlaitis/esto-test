1fa783e1878f01e47d950de19c6576c211880b6a
illia <illia.kyzlaitis.cv@gmail.com>
2020-05-12 10:47:34 +0300

    add transactions control so in case of corner case it is going to fine

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreTransactionRequest;
 use App\Http\Resources\TransactionResource;
 use App\Transaction;
+use Illuminate\Support\Facades\DB;
 
 class TransactionController extends Controller
 {
@@ -12,10 +13,18 @@ class TransactionController extends Controller
     {
         $data = $request['data']['attributes'];
 
-        $transaction = new Transaction($data);
 
-        $transaction->user()->associate(auth()->user());
-        $transaction->save();
+        try {
+            DB::beginTransaction();
+
+            $transaction = new Transaction($data);
+            $transaction->user()->associate(auth()->user());
+            $transaction->save();
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+        }
 
         return response()->json(new TransactionResource($transaction), 201);
     }
